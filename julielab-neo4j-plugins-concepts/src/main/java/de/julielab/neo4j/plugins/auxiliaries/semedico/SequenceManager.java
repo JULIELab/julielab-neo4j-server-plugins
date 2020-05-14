206c8777f57dbf50d244b9c92b91e71c42f08fe8
khituras <chew@gmx.net>
2020-05-14 09:23:24 +0200

    Fixed the SequenceManager for Neo4j4.

--- julielab-neo4j-plugins-concepts/src/main/java/de/julielab/neo4j/plugins/auxiliaries/semedico/SequenceManager.java
+++ julielab-neo4j-plugins-concepts/src/main/java/de/julielab/neo4j/plugins/auxiliaries/semedico/SequenceManager.java
@@ -1,81 +1,75 @@
 package de.julielab.neo4j.plugins.auxiliaries.semedico;
 
-import de.julielab.neo4j.plugins.datarepresentation.constants.NodeConstants;
 import org.neo4j.graphdb.*;
-import org.neo4j.graphdb.index.Index;
-import org.neo4j.graphdb.index.IndexHits;
+import org.neo4j.graphdb.schema.Schema;
 
-import java.util.NoSuchElementException;
+import java.util.Optional;
 
-import static de.julielab.neo4j.plugins.constants.semedico.SequenceConstants.NAME_SEQUENCES_ROOT;
 import static de.julielab.neo4j.plugins.datarepresentation.constants.NodeConstants.PROP_NAME;
 import static de.julielab.neo4j.plugins.datarepresentation.constants.NodeConstants.PROP_VALUE;
 
 public class SequenceManager {
 
-	private final static String SEQUENCE_INDEX = "sequenceIndex";
+    private final static String SEQUENCE_INDEX = "sequenceIndex";
 
-	static enum EdgeTypes implements RelationshipType {
-		HAS_SEQUENCE
-	}
+    public static int getNextSequenceValue(Transaction tx, String seqFacetGroup) {
+        Node sequence = getSequence(tx, seqFacetGroup);
+        tx.acquireReadLock(sequence);
+        tx.acquireWriteLock(sequence);
+        int currentSequenceValue = (Integer) sequence.getProperty(PROP_VALUE);
+        sequence.setProperty(PROP_VALUE, currentSequenceValue + 1);
+        return currentSequenceValue;
+    }
 
-	static enum Labels implements Label {
-		SEQUENCE_ROOT
-	}
+    static Node getSequenceRoot(Transaction tx) {
+        Optional<Node> sequenceRootOpt = tx.findNodes(Labels.SEQUENCE_ROOT).stream().findAny();
+        if (!sequenceRootOpt.isPresent()) {
+            Node seqRoot = tx.createNode(Labels.SEQUENCE_ROOT);
+            return seqRoot;
+        } else {
+            return sequenceRootOpt.get();
+        }
+    }
 
-	public static int getNextSequenceValue(Transaction tx, String seqFacetGroup) {
-			Node sequence = getSequence(tx, seqFacetGroup);
-			tx.acquireReadLock(sequence);
-			tx.acquireWriteLock(sequence);
-			int currentSequenceValue = (Integer) sequence.getProperty(PROP_VALUE);
-			sequence.setProperty(PROP_VALUE, currentSequenceValue + 1);
-			return currentSequenceValue;
-	}
+    private static synchronized Node getSequence(Transaction tx, String sequenceName) {
 
-	static Node getSequenceRoot(GraphDatabaseService graphDb) {
-		Index<Node> rootIndex = graphDb.index().forNodes(NodeConstants.INDEX_ROOT_NODES);
-		Node seqRoot = rootIndex.get(NodeConstants.PROP_NAME, NAME_SEQUENCES_ROOT).getSingle();
-		if (null == seqRoot) {
-			seqRoot = graphDb.createNode();
-			seqRoot.setProperty(PROP_NAME, NAME_SEQUENCES_ROOT);
-			rootIndex.add(seqRoot, PROP_NAME, NAME_SEQUENCES_ROOT);
-		}
-		return seqRoot;
-	}
+        try {
+            Node sequence = tx.findNode(Labels.SEQUENCE, PROP_NAME, sequenceName);
+            if (null == sequence) {
+                // First get or create the sequences super node.
+                // We do a connection not for lookup but just so the graph is
+                // connected
+                // and everything easily visible when viewing the graph e.g.
+                // with the
+                // Neo4j server graph viewer.
+                Node seqRoot = getSequenceRoot(tx);
+                sequence = tx.createNode();
+                sequence.setProperty(PROP_NAME, sequenceName);
+                sequence.setProperty(PROP_VALUE, 0);
+                seqRoot.createRelationshipTo(sequence, EdgeTypes.HAS_SEQUENCE);
+            }
+            return sequence;
+        } catch (MultipleFoundException e) {
+            throw new IllegalStateException("More than one sequence node for the sequence with name \"" + sequenceName + "\" was returned.", e);
+        }
+    }
 
-	private static synchronized Node getSequence(Transaction graphDb, String seqFacetGroup) {
+    /**
+     * Creates a schema index for the sequence nodes.
+     */
+    public static void createIndexes(Transaction tx) {
+        Schema schema = tx.schema();
+        if (!schema.getIndexes(Labels.SEQUENCE).iterator().hasNext()) {
+            schema.indexFor(Labels.SEQUENCE).withName(SEQUENCE_INDEX).on(PROP_NAME).create();
+        }
+    }
 
-		try {
-			IndexHits<Node> indexHits = null;
-			try //(Transaction tx = graphDb.beginTx()) 
-			{
-				Index<Node> seqIndex = graphDb.index().forNodes(SEQUENCE_INDEX);
-				indexHits = seqIndex.get(NodeConstants.PROP_NAME, seqFacetGroup);
-				Node sequence = indexHits.getSingle();
-				if (null == sequence) {
-					// First get or create the sequences super node.
-					// We do a connection not for lookup but just so the graph is
-					// connected
-					// and everything easily visible when viewing the graph e.g.
-					// with the
-					// Neo4j server graph viewer.
-					Node seqRoot = getSequenceRoot(graphDb);
-					sequence = graphDb.createNode();
-					sequence.setProperty(PROP_NAME, seqFacetGroup);
-					sequence.setProperty(PROP_VALUE, 0);
-					seqIndex.putIfAbsent(sequence, PROP_NAME, seqFacetGroup);
-					seqRoot.createRelationshipTo(sequence, EdgeTypes.HAS_SEQUENCE);
-//					tx.success();
-				}
-				return sequence;
-			} 
-			finally {
-				if (null != indexHits)
-					indexHits.close();
-			}
-		} catch (NoSuchElementException e) {
-			throw new IllegalStateException("More than one sequence node for the sequence with name \"" + seqFacetGroup + "\" was returned.", e);
-		}
-	}
+    private enum EdgeTypes implements RelationshipType {
+        HAS_SEQUENCE
+    }
+
+    private enum Labels implements Label {
+        SEQUENCE_ROOT, SEQUENCE
+    }
 
 }
